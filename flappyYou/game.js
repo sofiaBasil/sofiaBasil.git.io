4e9d1e84b80130a72cddc993e4130ac7374e4e21
Sofia Cioffi <scioffi@fordham.edu>
2020-11-16 18:46:03 -0500

    Initial commit

---
+++ flappyYou/game.js
@@ -0,0 +1,26 @@
+var score = 0;
+var bird_position = 0;
+var pipes = [
+    { 'x': }
+]
+
+const delta_pipe = 10; 
+
+function gameStart() {
+    //TODO: Start the game
+}
+
+//Game Loop
+function gameLoop() {
+    // Read Inputs
+    
+    // Update Positions
+
+    //Redraw Screen 
+    setTimeout(gameLoop, 10);
+}
+
+function gameOver() {
+    //TODO: Popup "Game Over" Screen 
+    //      Button to restart the game 
+}
